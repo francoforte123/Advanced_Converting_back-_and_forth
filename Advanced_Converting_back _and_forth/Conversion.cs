3facb66a83c4053a2c66a8b99e3ae174e000e7ba
Emmanuele La Duca <laducaemanuele20@gmail.com>
2023-06-21 14:11:42 +0200

    Aggiungere i file di progetto.

---
+++ Advanced_Converting_back _and_forth/Conversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Converting_back__and_forth
+{
+    internal class Conversion{
+
+        public void conversionCelsusToFahrenheit(double value1) {
+            double result = (value1 * 1.8) + 32;
+            Console.WriteLine("the conversion Celsus to Fahrenheit is= " + result);
+        }
+
+        public void conversionFahrenheitToCelsus(double value2)
+        {
+            double result = (value2 - 32) * 0.555;
+            Console.WriteLine("the conversion Fahrenheit to Celsus is= " + result);
+        }
+    }
+}
---
+++ Advanced_Converting_back _and_forth/Program.cs
@@ -0,0 +1,17 @@
+﻿using Advanced_Converting_back__and_forth;
+
+Conversion conversion = new Conversion();
+
+Console.Write("fai la tua scelta di conversione (C-F) oopure (F-C)= ");
+String choise= Console.ReadLine();
+
+if ( choise == "C-F"){
+    Console.Write("inserisci i gradi in Celsus= ");
+    int grade= int.Parse(Console.ReadLine());
+    conversion.conversionCelsusToFahrenheit(grade);
+}
+else if (choise == "F-C") {
+    Console.Write("inserisci i gradi in Fahrenheit= ");
+    double grade = double.Parse(Console.ReadLine());
+    conversion.conversionCelsusToFahrenheit(grade);
+}
